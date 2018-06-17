798fde285bcecef22c375c1436dc909e62423e80
Ana Karina De Ponte <31733847+anakarinadp@users.noreply.github.com>
2018-06-17 16:29:40 +0100

    Added welcome message

--- client.py
+++ client.py
@@ -5,17 +5,18 @@ import socket
 hote = "localhost"
 port = 12800
 
+print("On tente de se connecter au serveur...")
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_avec_serveur.connect((hote, port))
-print("Connexion établie avec le serveur sur le port {}".format(port))
+print("Connexion établie avec le serveur.")
 
-msg_a_envoyer = b""
-while msg_a_envoyer != b"fin":
-	msg_a_envoyer = input("> ")
-	msg_a_envoyer = msg_a_envoyer.encode()
-	connexion_avec_serveur.send(msg_a_envoyer)
-	msg_recu = connexion_avec_serveur.recv(1024)
-	print(msg_recu.decode())
+commencer = b""
+while commencer != b"c":
+	commande = input("> ")
+	commande = commande.encode()
+	connexion_avec_serveur.send(commande)
+	commencer = connexion_avec_serveur.recv(1024)
+	print(commencer.decode())
 	
 print("Fermeture de la connexion")
 connexion_avec_serveur.close()
